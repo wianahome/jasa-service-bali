3e4b3251bbbc4220bdb3da05ef0914e2665a97ba
Wiana <wianahome@gmail.com>
2026-04-27 15:17:50 +0800

    pasang tracker konversi JSB Wa

--- app/lib/google-ads.ts
+++ app/lib/google-ads.ts
@@ -2,7 +2,8 @@
 export const reportWaConversion = () => {
   if (typeof window !== 'undefined' && (window as any).gtag) {
     (window as any).gtag('event', 'conversion', {
-      'send_to': 'AW-17941029885/kISECL_ivaAcEP3H-epC', // Gunakan ID & Label yang sama untuk semua tombol WA
+      'send_to': 'AW-17941029885/WvYBCIrdsKMcEP3H-epC', // Gunakan ID & Label yang sama untuk semua tombol WA
     });
   }
 };
+
